c1e9c5ad7e7a140381e0c662cf4b922c55d77f58
selimdawa <selimdawa@gmail.com>
2026-06-23 19:09:31 +0300

    MainActivity Fix

--- app/src/main/java/com/littleapp/crypto/Activity/MainActivity.kt
+++ app/src/main/java/com/littleapp/crypto/Activity/MainActivity.kt
@@ -3,27 +3,18 @@ package com.littleapp.crypto.Activity
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.littleapp.crypto.R
 import com.littleapp.crypto.Unit.THEME
-import com.littleapp.crypto.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private var _binding: ActivityMainBinding? = null
-    private val binding get() = _binding!!
-
     private val activityContext: Context by lazy { this }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        //THEME.setThemeOfApp(activityContext)
+        THEME.setThemeOfApp(activityContext)
         super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
+        setContentView(R.layout.activity_main)
     }
 }
