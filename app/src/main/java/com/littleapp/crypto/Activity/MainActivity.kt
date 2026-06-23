1eaa5926982ed2f8427bf25b9ac5400a018420ad
selimdawa <selimdawa@gmail.com>
2026-06-23 21:01:20 +0300

    MainActivity Fix

--- app/src/main/java/com/littleapp/crypto/Activity/MainActivity.kt
+++ app/src/main/java/com/littleapp/crypto/Activity/MainActivity.kt
@@ -3,18 +3,20 @@ package com.littleapp.crypto.Activity
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.littleapp.crypto.R
+import com.littleapp.crypto.databinding.ActivityMainBinding
 import com.littleapp.crypto.Unit.THEME
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private val activityContext: Context by lazy { this }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         THEME.setThemeOfApp(activityContext)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
