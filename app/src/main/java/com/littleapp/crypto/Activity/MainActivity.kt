a22cea1d18da51004c7b24c0cae123997868abd0
selimdawa <selimdawa@gmail.com>
2026-08-29 06:57:23 +0300

    Edge to Edge - Add

--- app/src/main/java/com/littleapp/crypto/Activity/MainActivity.kt
+++ app/src/main/java/com/littleapp/crypto/Activity/MainActivity.kt
@@ -2,7 +2,10 @@ package com.littleapp.crypto.Activity
 
 import android.content.Context
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.littleapp.crypto.databinding.ActivityMainBinding
 import com.littleapp.crypto.utils.THEME
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,7 +19,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         THEME.setThemeOfApp(activityContext)
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
